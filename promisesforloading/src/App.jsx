6c2fd31c3a870eefebea1b9b0dbe2e8b637ed62c
Jose Salazar <jassalazar1995@gmail.com>
2024-02-04 21:13:41 -0500

    added some different styling

--- promisesforloading/src/App.jsx
+++ promisesforloading/src/App.jsx
@@ -29,17 +29,17 @@ function App() {
     });
   },[])
   
-
+ // Trying to understand different tailwind syntax
   return (
     <div>
-        <nav className="bg-gray-800 text-white p-4 flex justify-between items-center fixed top-0 left-0 right-0 z-40">
+        <nav className="bg-gray-800 text-white p-24 flex justify-between items-center fixed top-10 left-10 right-0 z-50">
       <div className="flex gap-4">
         <Link to='/home' className="hover:text-gray-300">
           <div>Home</div>
         </Link>
         </div>
     </nav>
-    <div className = "flex justify-center items-center h-screen bg-gradient-to-t from-indigo-500 to-blue-500">
+    <div className = "flex justify-center items-center h-screen bg-gradient-to-t from-indigo-400 to-blue-300">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
         <h1 className="text-center text-2xl font-bold">Order Status:</h1>
         <p className="text-center mt-4">{message}</p>
