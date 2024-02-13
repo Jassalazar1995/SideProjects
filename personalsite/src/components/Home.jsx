bfa7098d3bac1c74343cb32bdc7de55634b6b7f4
Jose Salazar <jassalazar1995@gmail.com>
2024-02-13 12:29:02 -0500

    changing the style of the website to be a single page. I think this will increase visibility of everything I want to show by minimizing the effort on the viewers part.

--- personalsite/src/components/Home.jsx
+++ personalsite/src/components/Home.jsx
@@ -1,3 +1,7 @@
+import About from "./About";
+import Contact from "./Contact";
+import Header from "./Header";
+import Projects from "./Projects";
 function Home() {
     return (
       <div className="max-w-4xl mx-auto mt-10">
@@ -5,6 +9,9 @@ function Home() {
         <p className="mt-5 text-lg text-gray-700 text-center">
           Dive into my projects and explore my skills in software development. Let's build something amazing together.
         </p>
+        <About />
+        <Projects />
+        <Contact />
       </div>
     );
   }
