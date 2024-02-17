06f7a087eedc331a2a3844a6551b72349d494234
Jose Salazar <jassalazar1995@gmail.com>
2024-02-17 00:32:20 -0500

    created the backend using spring initializr

---
+++ digitrecognizer/digit-recognizer/src/main/java/com/digitrecognizer/digitrecognizer/DigitRecognizerApplication.java
@@ -0,0 +1,13 @@
+package com.digitrecognizer.digitrecognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitRecognizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitRecognizerApplication.class, args);
+	}
+
+}
