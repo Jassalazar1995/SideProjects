175ab7ae36d7939169f4b6700c1bee2fcc486abc
Jose Salazar <jassalazar1995@gmail.com>
2024-02-19 23:59:47 -0500

    finished varibles section

---
+++ learningJava/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+	public static void main(String[] args) { 
+    // Final keyword makes variables immutable.
+      final double pi = 3.14;
+      System.out.println(pi);
+		
+	}
+}
---
+++ learningJava/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Jose";
+    int age = 28;
+    double desiredSalary =80000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
